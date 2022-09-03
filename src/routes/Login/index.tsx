668412a3aa4eef620dcad7b11c1d227fe9a0e2a2
yaserbayat <babak.kafka@gmail.com>
2022-09-03 12:48:27 +0430

    pre-commit test

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,35 +1,38 @@
-import React, {FormEvent} from 'react';
-import {useLocation, useNavigate} from "react-router-dom";
+import React, { FormEvent } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const navigate = useNavigate();
-    const location = useLocation()
-    const returnUrl = location.search.replace('?returnUrl=', '');
+  const navigate = useNavigate();
+  const location = useLocation();
+  const returnUrl = location.search.replace('?returnUrl=', '');
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const { elements } = e.target as HTMLFormElement;
-        const username = elements.namedItem('username') as HTMLInputElement;
-        const password = elements.namedItem('password') as HTMLInputElement;
-        let isLogeIin = true;
-        [...elements].forEach(elm => {
-            if (elm.tagName === 'INPUT') {
-                const input = elm as HTMLInputElement;
-                if (!input.value) isLogeIin = false;
-            }
-        })
-        if (isLogeIin) {
-            localStorage.accessToken = `${username.value}${password.value}`.split('').map(item => item + '$%^').join('')
-            navigate(returnUrl || '/')
-        }
-    };
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" name='username'/>
-            <input type="password" name='password'/>
-            <button type='submit'>Login</button>
-        </form>
-    );
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { elements } = e.target as HTMLFormElement;
+    const username = elements.namedItem('username') as HTMLInputElement;
+    const password = elements.namedItem('password') as HTMLInputElement;
+    let isLogeIin = true;
+    [...elements].forEach(elm => {
+      if (elm.tagName === 'INPUT') {
+        const input = elm as HTMLInputElement;
+        if (!input.value) isLogeIin = false;
+      }
+    });
+    if (isLogeIin) {
+      localStorage.accessToken = `${username.value}${password.value}`
+        .split('')
+        .map(item => item + '$%^')
+        .join('');
+      navigate(returnUrl || '/');
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="username" />
+      <input type="password" name="password" />
+      <button type="submit">Login</button>
+    </form>
+  );
 };
 
 export default Login;
