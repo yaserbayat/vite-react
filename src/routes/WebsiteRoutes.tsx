8244560d22ea4734ac934343da130137ccd97d45
yaserbayat <babak.kafka@gmail.com>
2022-09-03 12:58:35 +0430

    fix: add loading fallback to loadable components

--- src/routes/WebsiteRoutes.tsx
+++ src/routes/WebsiteRoutes.tsx
@@ -1,24 +1,29 @@
-import {Route, Routes} from "react-router-dom";
-import PrivateRoute from "src/components/PrivateRoute";
-import NotFound from "src/routes/NotFound";
-import React from "react";
-import loadable from "@loadable/component";
-const Home = loadable(() => import('src/routes/Website/Home'))
-const Product = loadable(() => import('src/routes/Website/Product'))
+import { Route, Routes } from 'react-router-dom';
+import PrivateRoute from 'src/components/PrivateRoute';
+import NotFound from 'src/routes/NotFound';
+import React from 'react';
+import loadable from '@loadable/component';
 
-const routes = [{element: <Home/>, path: '/'}, { element: <Product/>, path: '/product/:id'}]
+const fallback = <div>loading...</div>;
+const Home = loadable(() => import('src/routes/Website/Home'), { fallback });
+const Product = loadable(() => import('src/routes/Website/Product'), { fallback });
+
+const routes = [
+  { element: <Home />, path: '/' },
+  { element: <Product />, path: '/product/:id' },
+];
 
 const WebsiteRoutes = () => {
-    return (
-        <Routes>
-            {routes.map((route, index) => (
-                <Route key={JSON.stringify(route)} path={route.path} element={<PrivateRoute/>}>
-                    <Route  index={index === 0} path={route.path} element={route.element}/>
-                </Route>
-            ))}
-        <Route path='*' element={<NotFound/>}/>
+  return (
+    <Routes>
+      {routes.map((route, index) => (
+        <Route key={JSON.stringify(route)} path={route.path} element={<PrivateRoute />}>
+          <Route index={index === 0} path={route.path} element={route.element} />
+        </Route>
+      ))}
+      <Route path="*" element={<NotFound />} />
     </Routes>
-    );
+  );
 };
 
 export default WebsiteRoutes;
