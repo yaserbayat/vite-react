79e0eff94505e3ad3e5c66e4229c1423dda58c75
yaserbayat <babak.kafka@gmail.com>
2023-02-26 19:20:35 +0330

    fix: add routes

--- src/routes/Website/Product/index.tsx
+++ src/routes/Website/Product/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import DragAble from 'src/components/DragAndDrop';
 
 const Product = () => {
     return (
         <div>
-            This is product
+          <h2>Drag and drop to change the order of items</h2>
+            <DragAble>
+              {new Array(10).fill('').map((_item, index) => (
+                <div key={index} style={{padding: 12, border: '1px solid #ddd', backgroundColor: `rgba(0, 34, 0, ${Math.random()})`}}>item-{index}</div>
+              ))}
+            </DragAble>
         </div>
     );
 };
